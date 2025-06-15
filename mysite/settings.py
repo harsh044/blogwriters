bf546946b959fd3f80033f4ee3e40cec36da190c
Harshad <72267094+harsh044@users.noreply.github.com>
2025-06-15 10:49:40 +0530

    change debug mode

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%4oun*@x8hszh&mio*z@e_0*g^1=dwyq5g@8m^qz*f&$y7alm6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

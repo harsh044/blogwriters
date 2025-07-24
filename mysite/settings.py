beda95e7a3fc49518bd827714638561679a5f563
Harshad <72267094+harsh044@users.noreply.github.com>
2025-07-24 12:56:56 +0530

    update and fix the error

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    "jazzmin",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -89,17 +88,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'blogwriter',       
+#         'USER': 'root',             
+#         'PASSWORD': 'Root', 
+#         'HOST': 'localhost',        
+#         'PORT': '3306',             
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blogwriter',       
-        'USER': 'root',             
-        'PASSWORD': 'Root', 
-        'HOST': 'localhost',        
-        'PORT': '3306',             
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
